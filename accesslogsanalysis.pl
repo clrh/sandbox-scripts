03e21d8214b7c94e0c4fa97cb1280b8d80957152
Claire Hernandez <claire.hernandez@biblibre.com>
2011-12-06 10:56:20 +0100

    accesslog - adds date in report

--- accesslogsanalysis.pl
+++ accesslogsanalysis.pl
@@ -3,6 +3,10 @@
 use Modern::Perl;
 use Getopt::Long;
 use Data::Dumper;
+use List::Util qw(sum);  
+
+sub avg {     return sum(@_)/@_; }
+
 
 my $usage = <<EOF;
 accesslogsanalysis.pl - analyse apache acces logs to extract metrics.
@@ -10,6 +14,9 @@ accesslogsanalysis.pl - analyse apache acces logs to extract metrics.
     -f|--file           File to process
     -n|--nb             Nb top queries
     -v|--verbose        Print some debug informations
+
+    Analyses log type: 10.1.1.7 - - [01/Dec/2011:20:30:27 +0100] "GET /intranet-tmpl/prog/en/lib/jquery/plugins/ui.tabs.css HTTP/1.1" 304 - "http://koha.bm-limoges.fr:8200/cgi-bin/koha/catalogue/search.pl?q=test+apache" time=281
+    LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" time=%D"
 EOF
 my $help      = 0;
 my $file      = 0;
@@ -26,17 +33,27 @@ open FH, "<$file" or die "Can't open file $file: $!";
 
 my @calledget;
 my @calledpost;
+my @getTime;
+my @postTime;
 
 # Matches collection
 
 while (my $logline = <FH>) {
-  if ($logline =~ /GET (.+) HTTP/) {                # find queries "... opac-search.pl: OpacSolrSimpleSearch:q=int_authid:136226: at /home/koha..."
-    push @calledget, $1;
+  if ($logline =~ /- - \[(.*) \+0100\] "GET (.+) HTTP.*time=([0-9]+).*$/) {
+    push @calledget, $2;
+    push @getTime, {d=> $1,q =>$2,t=>$3};
   }
-  if ($logline =~ /POST (.+) HTTP/) {                # find queries "... opac-search.pl: OpacSolrSimpleSearch:q=int_authid:136226: at /home/koha..."
-    push @calledpost, $1;
+  if ($logline =~ /- - \[(.*) \+0100\] "POST (.+) HTTP.*time=([0-9]+).*$/) {
+    push @calledpost, $2;
+    push @postTime, {d=>$1,q =>$2,t=>$3};
   }
 }
+
+@getTime = sort { $b->{t} <=> $a->{t} } @getTime;
+my $avggettime = avg (map {$_->{t}} @getTime);
+@postTime = sort { $b->{t} <=> $a->{t} } @postTime;
+my $avgposttime = avg (map {$_->{t}} @postTime);
+
 
 # Find top called scripts
 
@@ -56,6 +73,19 @@ say "\nTop $nb post called";
 for (0..$nb-1) {
   $q = $sortedqueries[$_]; 
   say $_+1 . "\t" . $topqueries{$q} . "\t" . $q;
+}
+
+my $divise = 1000000;
+say "\nTop $nb time Get apache calls - nb ".scalar @getTime;
+say "Get average(sec): ". $avggettime/$divise;
+for (0..$nb-1) {
+  say $getTime[$_]{'d'} . "\t" . $getTime[$_]{'t'}/$divise . "\t". $getTime[$_]{'q'};
+}
+
+say "\nTop $nb time Post apache calls - nb ".scalar @postTime;
+say "Post average (sec): ". $avgposttime/$divise ;
+for (0..$nb-1) {
+  say $postTime[$_]{'d'}. "\t" .  $postTime[$_]{'t'}/$divise . "\t". $postTime[$_]{'q'};
 }
 
 close FH;
